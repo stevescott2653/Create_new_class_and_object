567de6752f82c98e29ec1a93f83d668338c735ab
Steven Scott <stevescott2653@gmail.com>
2019-09-30 16:58:42 -0400

    Add files via upload

---
+++ Create_new_class_and_object/Create_new_class_and_object/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_new_class_and_object
+{
+    public class Student
+    {
+        public int id;
+
+        public String name;
+    }
+
+    class TestStudent
+    {
+        public static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.id = 101;
+            s1.name = "John Doe";
+            Console.WriteLine(s1.id);
+            Console.WriteLine(s1.name);
+
+        }
+    }
+}
